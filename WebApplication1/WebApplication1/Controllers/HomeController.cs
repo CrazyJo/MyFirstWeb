a9c5dc5f808e8e2f3c8ad957145e7bbec7e63af6
Dmitry Vakulenko <dVakulen@users.noreply.github.com>
2016-07-10 18:03:45 +0300

    Update HomeController.cs

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication1.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "My application description page.";
 
             return View();
         }
